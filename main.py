f669f70313fb4de1785ed11f3b59f670e7922f13
AbAeterno <alitomosca@gmail.com>
2026-02-18 13:05:33 -0300

    Remove pathlib import

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from typing import List
-import os, pathlib
+import os
 
 # Pydantic schema definitions
 class InventoryItem(BaseModel):
